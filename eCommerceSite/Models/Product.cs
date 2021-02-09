35d89dba3a562f4ef9c1afbf60a9c9c961a5295e
Sebastian <66985210+sebshev@users.noreply.github.com>
2021-02-09 01:11:57 -0800

    Issue#5 create product db (#8)
    * Created Product model
    
    * Set up DB context
    
    * Changed indentation
    
    * Added DbContext in Startup, an dinstalled SQL Server NuGet package
    
    * Added connection string for DB to .json file
    
    * Added EntityFrameworkCore.Tools form NuGet package manager
    
    * Asdded migration to create DB with Products table

---
+++ eCommerceSite/Models/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Models
+{
+    /// <summary>
+    /// A sellable product 
+    /// </summary>
+    public class Product
+    {
+        /// <summary>
+        /// The unique identifier for the product
+        /// also the Primary Key
+        /// </summary>
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// The name of the product
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The retail price in US currency
+        /// </summary>
+        public double Price { get; set; }
+
+        /// <summary>
+        /// Category for the product
+        /// EX. Electronics, Clothes ect.
+        /// </summary>
+        public string Category { get; set; }
+    }
+}
