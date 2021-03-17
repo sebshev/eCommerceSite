64965ac350fe79928699c638d5e45b4bc37ba692
Sebastian Shevlin <seb112697@gmail.com>
2021-03-17 14:25:29 -0700

    Created register view model

--- eCommerceSite/Models/UserAccount.cs
+++ eCommerceSite/Models/UserAccount.cs
@@ -8,6 +8,9 @@ namespace eCommerceSite.Models
 {
     public class UserAccount
     {
+        /// <summary>
+        /// HTe unique user Id for each user
+        /// </summary>
         [Key]
         public int UserId { get; set; }
 
@@ -17,6 +20,26 @@ namespace eCommerceSite.Models
 
         public string Password { get; set; }
 
+        public DateTime? DateOfBirth { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Compare(nameof(Email))]
+        public string ConfirmEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+
+        [DataType(DataType.Date)] // Time is ignored
         public DateTime? DateOfBirth { get; set; }
     }
 }
