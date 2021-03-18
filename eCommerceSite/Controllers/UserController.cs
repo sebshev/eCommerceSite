1047e65ce65d9dbbb4073735592b6cd42cb209eb
Sebastian Shevlin <seb112697@gmail.com>
2021-03-18 13:10:39 -0700

    Add registration for user into DB is working

--- eCommerceSite/Controllers/UserController.cs
+++ eCommerceSite/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,40 @@ namespace eCommerceSite.Controllers
 {
     public class UserController : Controller
     {
+        private readonly ProductContext _context;
+
+        public UserController(ProductContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel reg)
+        {
+            if (ModelState.IsValid)
+            {
+                // map data to useraccount object
+                UserAccount account = new UserAccount()
+                {
+                    DateOfBirth = reg.DateOfBirth,
+                    Email = reg.Email,
+                    Username = reg.Username, 
+                    Password = reg.Password
+                };
+
+                // add to DB
+                _context.UserAccounts.Add(account);
+                await _context.SaveChangesAsync();
+                // redirect to home page
+                return RedirectToAction("Index", "Home");
+            }
+            return View(reg);
         }
     }
 }
--- eCommerceSite/Models/UserAccount.cs
+++ eCommerceSite/Models/UserAccount.cs
@@ -28,6 +28,10 @@ namespace eCommerceSite.Models
     public class RegisterViewModel
     {
         [Required]
+        [StringLength(20)]
+        public string Username { get; set; }
+
+        [Required]
         [EmailAddress]
         [StringLength(200)]
         public string Email { get; set; }
