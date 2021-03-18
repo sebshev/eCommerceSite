243897389aa2276eda15814a5c030a6b67243974
Sebastian Shevlin <seb112697@gmail.com>
2021-03-18 14:37:00 -0700

    Added login custom error message

--- eCommerceSite/Controllers/UserController.cs
+++ eCommerceSite/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using eCommerceSite.Data;
 using eCommerceSite.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -49,6 +50,39 @@ namespace eCommerceSite.Controllers
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            //UserAccount account = await (from u in _context.UserAccounts
+            //                             where u.Username == model.UsernameOrEmail
+            //                                || u.Email == model.UsernameOrEmail
+            //                                && u.Password == model.Password
+            //                             select u).SingleOrDefaultAsync();
+
+            UserAccount account = await _context.UserAccounts
+                        .Where(userAcc => (userAcc.Username == model.UsernameOrEmail ||
+                                            userAcc.Email == model.UsernameOrEmail) &&
+                                            userAcc.Password == model.Password)
+                        .SingleOrDefaultAsync();
+            if (account == null)
+            {
+                // Credentials did not match
+                // Error Message
+                ModelState.AddModelError(string.Empty, "Credentials were not found");
+
+                return View(model);
+            }
+
+            //Log user into website
+            return RedirectToAction("Index", "Home");
+
+
         }
     }
 }
