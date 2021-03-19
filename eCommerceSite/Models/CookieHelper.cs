d3431ab44f64867174e914e84feb63c3f8315438
Sebastian <66985210+sebshev@users.noreply.github.com>
2021-03-18 21:35:34 -0700

    Addded number to display number of items in cart (#47)

--- eCommerceSite/Models/CookieHelper.cs
+++ eCommerceSite/Models/CookieHelper.cs
@@ -49,7 +49,8 @@ namespace eCommerceSite.Models
 
         public static int GetTotalCartProducts(IHttpContextAccessor http)
         {
-            throw new NotImplementedException();
+            List<Product> cartProducts = GetCartProducts(http);
+            return cartProducts.Count;
         }
     }
 }
