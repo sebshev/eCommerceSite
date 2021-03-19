af94e2ccda9889c428ec24887c82e59f2c3dbd01
Sebastian <66985210+sebshev@users.noreply.github.com>
2021-03-18 21:26:19 -0700

    Issue#35 shopping cart (#46)
    * Added shopping cart icon
    
    * Added shopping cart link to nav and add to cart button on catalog
    
    * A single product can now be added to cart
    
    * Able to store multiple items in cart
    
    * Created summary page (does not work with empty cookie)
    
    * Refactored cart controller
    
    * Added message for empty cart summary with link to catalog

---
+++ eCommerceSite/Controllers/CartController.cs
@@ -0,0 +1,49 @@
+﻿using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ProductContext _context;
+        private readonly IHttpContextAccessor _httpContext;
+
+        public CartController(ProductContext context, IHttpContextAccessor httpContext)
+        {
+            _context = context;
+            _httpContext = httpContext;
+        }
+
+        /// <summary>
+        /// Adds a product to the shopping cart
+        /// </summary>
+        /// <param name="id">The Id of the product to store</param>
+        /// <returns></returns>
+        public async Task<IActionResult> Add(int id) // Id of the product to add
+        {
+            // Get from DB
+            Product p = await ProductDb.GetProductAsync(_context, id);
+
+            CookieHelper.AddProductToCart(_httpContext, p);
+
+            // redirect back to the same page of catalog
+            return RedirectToAction("Index", "Product");
+        }
+
+        public IActionResult Summary()
+        {
+            // Display all products in shopping cart cookie
+
+            List<Product> cartProducts = CookieHelper.GetCartProducts(_httpContext);
+            return base.View(cartProducts);
+        }
+    }
+}
